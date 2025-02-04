e23ea01ac469261588df728d28d5f871e0fec103
Lívia de Camargo <livia.camargo122@gmail.com>
2025-02-04 11:17:03 -0300

    ADD: cadastro de frases

--- app.py
+++ app.py
@@ -11,15 +11,34 @@ lista_cores = ["#02074a",
             "#020aa3"]
 
 # Lista de imagens para alterar na pag inicial
-imagem_aleatoria = ["baiacu.jpeg"
+imagem_aleatoria = ["baiacu.jpg",
+                    "lesma-do-mar.jpg",
+                    "peixe-morcego.jpg",
+                    "polvo-azuis.jpg"
+]
 
+frase_aleatoria = [ "Cada espécie marinha tem um papel único que mantém o equilíbrio ecológico do nosso planeta.",
+                   "Os ecossistemas marinhos ajudam a regular o clima, absorvendo grandes quantidades de carbono.",
+                   "A perda de biodiversidade nos oceanos afeta diretamente as populações humanas que dependem do mar para sustento.",
+                   "Os animais marinhos são fundamentais para o equilíbrio dos ecossistemas oceânicos."
 ]
 
 # Aqui ira ficar todas as minhas rotas
+@app.route("/inicial")
+def pagina_inicial():
+    cor_de_fundo = random.choice(lista_cores)
+    return render_template("PaginaInicial.html", cor_de_fundo_html = cor_de_fundo)
+
 @app.route("/")
 def pagina_sobre():
+    imagem_random = random.choice(imagem_aleatoria)
     cor_de_fundo = random.choice(lista_cores)
-    return render_template("PaginaSobre.html", cor_de_fundo_html = cor_de_fundo)
+    frase_random = random.choice(frase_aleatoria)
+    return render_template("PaginaSobre.html", imagem_random_html = imagem_random, cor_de_fundo_html = cor_de_fundo, frase_random_html = frase_random)
+
+@app.route("/cadastro")
+def pagina_cadastro():
+    return render_template("CadastroFrases.html", frases = frase_aleatoria)
 
 
 app.run(debug=True, host="0.0.0.0", port=8080)
